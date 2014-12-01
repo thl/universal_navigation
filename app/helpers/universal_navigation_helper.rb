b5c59d7939b65e0037f5f257ce347e4db48fda7a
Andres Montano <amontano@virginia.edu>
2014-12-01 12:44:19 -0600

    converting code to shanti theme

--- app/helpers/universal_navigation_helper.rb
+++ app/helpers/universal_navigation_helper.rb
@@ -6,7 +6,8 @@ module UniversalNavigationHelper
         :title => "Places",
         :app => :places,
         :url_method => :kmaps_url,
-        :count_method => :feature_count
+        :count_method => :feature_count,
+        :shanticon => 'places'
       }]
     array <<
       {
@@ -15,6 +16,7 @@ module UniversalNavigationHelper
         :app => :topics,
         :url_method => :topical_map_url,
         :count_method => :category_count,
+        :shanticon => 'subjects'
         #:count_method_args => {:cumulative => true}
       } if defined?(SubjectsIntegration)
     array = array + [
@@ -24,7 +26,8 @@ module UniversalNavigationHelper
         :app => :media,
         :url_method => :pictures_url,
         :count_method => :media_count,
-        :count_method_args => {:type => 'Picture'}
+        :count_method_args => {:type => 'Picture'},
+        :shanticon => 'photos'
       },
       {
         :id => :videos,
@@ -32,7 +35,8 @@ module UniversalNavigationHelper
         :app => :media,
         :url_method => :videos_url,
         :count_method => :media_count,
-        :count_method_args => {:type => 'Video'}
+        :count_method_args => {:type => 'Video'},
+        :shanticon => 'video'
       },
       {
         :id => :documents,
@@ -40,36 +44,18 @@ module UniversalNavigationHelper
         :app => :media,
         :url_method => :documents_url,
         :count_method => :media_count,
-        :count_method_args => {:type => 'Document'}
+        :count_method_args => {:type => 'Document'},
+        :shanticon => 'sources'
       }
     ]
   end
   
-  def primary_tabs_list_items(current_tab_id)
-    primary_tabs_list.collect{|tab|
-      tab_id = tab[:id]
-      if current_tab_id == tab_id
-        "<li class='ui-state-active ui-tabs-selected'>
-          <a><span>#{tab[:title]}</span></a>
-        </li>"
-      else
-        "<li class='ui-tabs-unselected'>
-          <a href='#{tab[:url]}'><span>#{tab[:title]}</span></a>
-        </li>"
-      end
-    }.join("\n\t\t").html_safe
-  end
-
-  #
-  # The custom_secondary_tabs argument should be an array like [[:place, "Place", feature_url(f.fid)],
-  # [:related, "Related", related_features_url(f)]]
-  #
-  def secondary_tabs_list_items(current_app_id, current_tab_id, custom_secondary_tabs)
+  def active_secondary_tabs_list(current_app_id, current_tab_id, custom_secondary_tabs)
     @tab_options ||= {}
     if current_app_id == :dictionary or current_app_id== :himalayan_search
-      tabs = custom_secondary_tabs
+      tabs = custom_secondary_tabs || []
     else
-       tabs = [[:home, "Home", "#{ActionController::Base.config.relative_url_root}/"]] + custom_secondary_tabs
+       tabs = [[:home, "Home", "#{ActionController::Base.config.relative_url_root}/", 'grid']] + custom_secondary_tabs
     end
     unless @tab_options[:entity].blank?
       entity = @tab_options[:entity]
@@ -85,45 +71,44 @@ module UniversalNavigationHelper
             else
               count = entity.send(tab[:count_method], tab[:count_method_args])
             end
-            tabs << [tab[:id], "#{tab[:title]} (#{count})", url] if count > 0
+            tabs << [tab[:id], "#{tab[:title]} (#{count})", url, tab[:shanticon]] if count > 0
           end
         end
       }
     end
-      
-    tabs.collect{|tab|
+    tabs
+  end
+  
+  #
+  # The custom_secondary_tabs argument should be an array like [[:place, "Place", feature_url(f.fid)],
+  # [:related, "Related", related_features_url(f)]]
+  #
+  def secondary_tabs_list_items(current_app_id, current_tab_id, custom_secondary_tabs)
+    active_secondary_tabs_list(current_app_id, current_tab_id, custom_secondary_tabs).collect do |tab|
       tab_id = tab[0]
       title = tab[1]
-      url = tab[2]
+      shanticon = tab[3]
+      # url = tab[2] USE THIS FOR JS
       if current_tab_id == tab_id
-        "<li class='ui-state-active ui-tabs-selected'>
-          <a href='#{url}' id=uitab_#{title}><span>#{title}</span></a>
+        "<li class=\"#{tab_id}\ active\">
+          <a href=\"\##{tab_id}\" data-toggle=\"pill\"><i class=\"icon shanticon-#{shanticon}\"></i>#{title}</a>
         </li>"
       else
-        "<li class='ui-tabs-unselected'>
-          <a href='#{url}' id=uitab_#{title}><span>#{title}</span></a>
+        "<li class=\"#{tab_id}\">
+          <a href='\##{tab_id}' data-toggle=\"pill\"><i class=\"icon shanticon-#{shanticon}\"></i>#{title}</a>
         </li>"
       end
-    }.join("\n\t\t").html_safe
+    end.join("\n").html_safe
   end
 
   #
   # Returns the HTML for creating the tabs.  Currently only accepts a block instead of a string
   #
-  def universal_navigation(*args, &block)
-    if block_given?
-      primary_tab_id = args[0]
-      secondary_tab_id = args[1]
-      custom_secondary_tabs = args[2] || []
-      options = args[3] || {}
-      ("<div id='universal_navigation' class='ui-tabs'>
-      		<ul class='primary-tabs ui-tabs-nav'>
-      			#{primary_tabs_list_items primary_tab_id}
-      		</ul>
-      		<ul class='secondary-tabs ui-tabs-nav'>
-      			#{secondary_tabs_list_items primary_tab_id, secondary_tab_id, custom_secondary_tabs}
-      		</ul>" + content_tag(:div, :class => 'ui-tabs-panel', :id => 'universal_navigation_content', &block) +
-      "</div>").html_safe
-    end
+  def universal_navigation(*args)
+    "<aside class=\"content-resources col-xs-6 col-sm-3 sidebar-offcanvas equal-height\">
+    		<ul class=\"nav nav-pills nav-stacked\">
+    			#{secondary_tabs_list_items *args}
+    		</ul>
+     </aside>".html_safe
   end
 end
